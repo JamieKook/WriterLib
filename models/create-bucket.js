016d136dca2df39e685587252968054760f71103
James <jwtankersley10@gmail.com>
2020-02-11 19:20:24 -0600

    created bucket in AWS

---
+++ models/create-bucket.js
@@ -0,0 +1,22 @@
+const AWS = require('aws-sdk');
+const ID = process.env.ID;
+const SECRET = process.env.SECRET;
+
+const BUCKET_NAME = process.env.BUCKET_NAME;
+
+const s3 = new AWS.S3({
+    accessKeyId: ID,
+    secretAccessKey: SECRET
+});
+
+const params = {
+    Bucket: BUCKET_NAME,
+    CreateBucketConfiguration: {
+        LocationConstraint: "eu-west-1"
+    }
+};
+
+s3.createBucket(params, function(err, data) {
+    if (err) console.log(err, err.stack);
+    else console.log('Bucket Created Successfully', data.location);
+});
