ea349a711df3251b8b0773df9652e8b11a53d72a
Jamie <kookjc6@gmail.com>
2020-02-12 21:30:48 -0600

    added in pdfsplit and adjusted post so that incoming file is saved in temp folder, parsed into image files where the file paths are returned to be rendered and then removes all files after completion

--- public/js/addBook.js
+++ public/js/addBook.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
       $.ajax({
         type: "POST",
         enctype: 'multipart/form-data',
-        url: "/api/books",
+        url: "/api/books/fileUpload",
         data: data,
         processData: false,
         contentType: false,
@@ -33,56 +33,4 @@ $(document).ready(function() {
     });
     });
 });
-    // // Getting references to our form and input
-    // const bookForm = $("form.addBook");
-    // const titleInput = $("input#bookTitle");
-    // const genreInput = $("input#bookGenre");
-  
-   
-    
-  
-    // // When the signup button is clicked, we validate the email and password are not blank
-    // bookForm.on("submit", function(event) {
-    //   event.preventDefault();
-     
-    //   const file =bookFile[0].files[0];
-    // //   const reader = new FileReader(); 
-    // //   reader.read
-
-
-    //   const bookData = {
-    //     title: titleInput.val().trim(),
-    //     genre: genreInput.val().trim(),
-    //     file: bookFile[0].files[0] 
-    //   };
-  
-
-    //   if (!bookData.title|| !bookData.genre|| !bookData.file) {
-    //     return;
-    //   }
-      
-    
-    //   // addBook(bookData.title, bookData.genre, bookData.file);
-    //   titleInput.val("");
-    //   genreInput.val("");
-    //   bookFile.val(""); 
-    // });
-  
-    // f
-    // // Does a post to the signup route. If successful, we are redirected to the members page
-    // // Otherwise we log any errors
-    // // function addBook(title, genre, file) {
-    // //   $.post("/api/books", {
-    // //     title: title,
-    // //     genre: genre,
-    // //     file: file
-    // //   })
-    // //     .then(function(data) {
-    // //     //   authorData.UserId = data.id; 
-    // //     //   addAuthorData(authorData); 
-    // //       // If there's an error, handle it by throwing up a bootstrap alert
-    // //     })
-    // //     .catch(handleLoginErr);
-    // // }
-
   
--- routes/book-api-routes.js
+++ routes/book-api-routes.js
@@ -3,6 +3,8 @@ const isAuthenticated = require("../config/middleware/isAuthenticated");
 const path = require("path"); 
 const AwsHandling = require("../aws/awsHandling"); 
 const awsHandling = new AwsHandling();
+const PdfHandling = require("../pdfsplit"); 
+const pdfHandling = new PdfHandling(); 
 
 module.exports = function(app) {
 
@@ -26,26 +28,27 @@ module.exports = function(app) {
         });
     }); 
 
-    app.post("/api/books", async function(req, res) {
+    app.post("/api/books/fileUpload", async function(req, res) {
         try {
             const results = await db.Book.create({
                 title: req.body.title,
-                genre: req.body.genre
+                genre: req.body.genre,
                 }); 
-            const bookId = results.id; 
+            const bookId = results.id;  
             if(!req.files) {
                 res.send({
                     status: false,
                     message: "No file uploaded",
                     json: results
-
+ 
                 }); 
             } else {
                 let bookFile = req.files.bookFile; 
-                await bookFile.mv(`./uploads/${bookId}/`+ bookFile.name);
-               let file = path.join(__dirname,`../uploads/${bookId}/${bookFile.name}`); 
-               console.log(file); 
-               await awsHandling.upload(file, bookId);
+                bookFile.mv(`./tmp/${bookId}/book${bookId}.pdf`);
+                await awsHandling.upload(file, bookId);
+                const imgPaths = await pdfHandling.createImages(bookId);
+                console.log(imgPaths); 
+                //add render function here to use image path object as code
                 res.send({
                     status: true,
                     message: "File is uploaded",
@@ -55,6 +58,7 @@ module.exports = function(app) {
                         size: bookFile.size
                     }
                 }); 
+                pdfHandling.deleteTempBookFolder(bookId); 
             }
             // res.status(200); 
             // res.json(results); 
