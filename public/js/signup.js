ed09c491f1fb9371e493988e763bbc74031f312a
Jamie <kookjc6@gmail.com>
2020-02-21 21:49:18 -0600

    running console for submit button on sign up

--- public/js/signup.js
+++ public/js/signup.js
@@ -11,6 +11,7 @@ $(document).ready(function() {
   // When the signup button is clicked, we validate the email and password are not blank
   signUpForm.on("submit", function(event) {
     event.preventDefault();
+    console.log("testing"); 
     const userData = {
       email: emailInput.val().trim(),
       password: passwordInput.val().trim()
